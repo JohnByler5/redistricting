New weight calculation & heuristic algorithm, other algorithm fixes, algorithm testing, and much more

redistricting_env.py
@@ -27,7 +27,7 @@ def refresh_data():
 
 
 class RedistrictingEnv:
-    def __init__(self, data_path, n_districts, live_plot=False, save_dir=None):
+    def __init__(self, data_path, n_districts, live_plot=False, save_data_dir=None, save_img_dir=None):
         self.data = gpd.read_parquet(data_path)
         num_cols = self.data.select_dtypes(np.number).columns
         self.data[num_cols] = self.data[num_cols].astype(np.float64)
@@ -46,5 +46,6 @@ class RedistrictingEnv:
         self.union_cache = UnionCache(geometries=self.data.geometry, size=1_000)
 
         self.live_plot = live_plot
-        self.save_dir = save_dir
+        self.save_data_dir = save_data_dir
+        self.save_img_dir = save_img_dir
         self.fig, self.ax = plt.subplots(figsize=(10, 10))